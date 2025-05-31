fad629424d96fb41703324272aa96f205518675d
devsazzadhossain <sazzadhossainstd01@gmail.com>
2025-05-31 19:15:45 +0600

    Practiced php syntax, declaring php variables - by devsazzadhossain

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    $fonts = "Roboto";
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +10,69 @@
     <title>PHP Fundamentals</title>
 </head>
 <body>
-    <?php echo "Hello, PHP World!"; ?>
+
+<style>
+    *{
+        margin: 0px;
+        padding: 0px;
+    }
+    body {
+        font-family: <?php echo $fonts;?>;
+        margin-top: <?php echo "20px";?>;
+    }
+    h1 {
+        font-family: <?php echo $fonts;?>;
+    }
+    div.php-coding {
+        width: 900px;
+        margin: 0 auto;
+        background-color: #dddddd;
+        border-radius: 10px;
+    }
+    div.header, footer.footer-area {
+        padding: 20px 30px;
+        background-color: <?php echo "#1E1E1E";?>;
+        color: #ffffff;
+        text-align: center;
+    }
+    a{
+        color: #ffffff;
+    }
+    div.main-content {
+        min-height: 300px;
+        padding: 30px;
+    }
+
+</style>
+
+    <div class="php-coding">
+
+        <div class="header">
+            <h1><?php echo "PHP Fundamentals";?></h1>
+        </div>
+
+        <div class="main-content">
+
+            <?php echo "Hello, PHP World!"; ?> <br>
+            <h1><?php echo "Hello, PHP World";?></h1><br>
+            <?php echo "Hello, php World"; ?><br>
+
+            <?php 
+                $a = "5";
+                $b = "10";
+                $c = $a+$b;
+                echo "total value = ".$c." ok fine";
+            ?>
+
+        </div>
+
+        <footer class="footer-area">
+            <a href="https://github.com/sazzadhossainmit"><?php echo "https://github.com/sazzadhossainmit";?></a>
+        </footer>
+
+
+    </div>
+
+
 </body>
 </html>
