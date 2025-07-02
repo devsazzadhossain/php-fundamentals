f610729e9e7505c791d00bbda74a49ee0b49ccb1
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-07-02 22:11:56 +0600

    Practiced PHP file create/write

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Fundamentals</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -53,27 +54,17 @@
 
     <div class="main-content">
 
-        <h2>PHP File handling</h2><hr/><br/>
-
-        <!-- readfile()  -->
-        <?php
-            // echo readfile("test.txt"). "<br>"; 
-        ?>  
-
-        <!-- File Open/Read/Close  -->
-        <?php
-            // $myfile = fopen("test.txt", "r") or die("Unable to open file!!");
-            // echo fread($myfile, filesize("test.txt")); Reads an open file
-            // fclose($myfile); 
-        ?>
+        <h2>PHP File create & write</h2><hr/><br/>
 
         <?php
-            $myfile = fopen("test.txt", "r") or die("Unable to open file!!");
-            while (!feof($myfile)){
-                // echo fgetc($myfile). "<br>"; // Gets the first character of a file
-                echo fgets($myfile). "<br>"; // Gets the first line of a file
-            }
-            fclose($myfile);
+
+            $createfile = fopen("new.txt", "w") or die("Unable to create file!");
+            $one = "This is the first line\n";
+            fwrite($createfile, $one); // The fwrite() function is used to write to a file.
+            $two = "This is the second line\n";
+            fwrite($createfile, $two); 
+            fclose($createfile);
+            
         ?>
         
         
