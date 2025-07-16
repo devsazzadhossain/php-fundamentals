cefd6b4fdbc8ed64a4d0fbc38d04389855eeb394
devsazzadhossain <sazzadhossainstd01@gmail.com>
2025-07-16 12:05:14 +0600

    PHP filter advanced - devsazzadhossain

--- index.php
+++ index.php
@@ -54,63 +54,29 @@
 
     <div class="main-content">
 
-        <h2>PHP Filters</h2><hr/><br/>
+        <h2>PHP Filters Advanced</h2><hr/><br/>
 
-        <!-- The filter_list() function can be used to list what the PHP filter extension offers: -->
-        <table>
-            <tr>
-                <td>Filter Name</td>
-                <td>Filter ID</td>
-            </tr>
-            <?php
-                foreach (filter_list() as $id => $filter) {
-                    echo "<tr><td>". $filter . "</td><td>". filter_id($filter). "</td></tr>";
-                }
-            ?>
-        </table>
-
-        <!-- The filter_var() function both validate and sanitize data. -->
         <?php
-            $int = 50.6;
-            if (filter_var($int, FILTER_VALIDATE_INT)){
-                echo "$int is valid Integer";
-            } else {
-                echo "$int is not valid Integer!". "<br>";
-            }
-        ?>
+            $intnum = 199;
+            $min = 1;
+            $max = 200;
 
-        <!-- Validate IP -->
-        <?php
-            $ip = "127.0.0.1";
-            if (filter_var($ip, FILTER_VALIDATE_IP)){
-                echo "$ip is valid IP.";
-            } else {
-                echo "$ip is not valid IP!" . "<br>";
+            if (filter_var($intnum, FILTER_VALIDATE_INT, array("options" => array("min_range" => $min, "max_range" => $max)))){
+                echo "$intnum is a Valid integer";
+            }   else {
+                echo "$intnum is an Invalid integer detected! Please type within the range.";
             }
         ?><br>
 
-        <!-- Validate Email -->
         <?php
-            $email = "sazzadhossainmitgmail.com";
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
-                echo "$email is valid email.";
+            $url = "https://example.com/index.html?q=123";
+            if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED)){
+                echo "$url is a Valid query url";
             } else {
-                echo "$email is not valid email!". "<br>";
+                echo "$url is an Invalid query url, no query string found.";
             }
         ?>
-
-        <!-- Validate URL -->
-        <?php
-            // Remove all illegal characters from email (SANITIZE)
-            // $url = filter_var($url, FILTER_SANITIZE_EMAIL);
-
-            $url = "https://github.com/sazzadhossainmit";
-            if (filter_var($url, FILTER_SANITIZE_URL)){
-                echo "$url is valid URL address.";
-            } else {
-                echo "$url is not valid URL address!". "<br>";
-            }
-        ?>
+        
 
     </div>
 
